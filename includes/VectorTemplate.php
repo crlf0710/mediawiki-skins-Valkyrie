cf2c775252e37db86302eda806531f85c2fc57d0
jdlrobson <jdlrobson@gmail.com>
2020-05-08 07:20:12 -0700

    Don't "break" the variants by breaking early
    Hope you enjoyed the pun, as this is an unfortunate bug to have
    slipped into production.
    
    While previously we breaked after finding the selected items
    prior to I098e6921e8f7ef65dacacf09b9c25f70c945e58e we shouldn't do
    that any more as we're using the same loop to iterate through and
    generate our items!
    
    Bug: T251521
    Change-Id: I92c28e8fba684d7977dfd207ff939ac07a7a07af
    (cherry picked from commit 01dbda42cab9b62b9f405ec05a9034016d3c12ed)

--- includes/VectorTemplate.php
+++ includes/VectorTemplate.php
@@ -475,7 +475,6 @@ class VectorTemplate extends BaseTemplate {
 			if ( $setLabelToSelected ) {
 				if ( isset( $item['class'] ) && stripos( $item['class'], 'selected' ) !== false ) {
 					$props['label'] = $item['text'];
-					break;
 				}
 			}
 		}
